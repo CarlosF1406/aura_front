5daec6d132c9f8ef64a9632a42c4d2ad285e3c11
MrWisz <eduardoluis09@hotmail.com>
2024-10-08 21:43:52 -0400

    adición de imports a los componentes

---
+++ UserComponents/LoginComponents/LoginForm.tsx
@@ -0,0 +1,5 @@
+import {Button, ButtonGroup} from "@nextui-org/button";
+import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/card";
+import {Input} from "@nextui-org/input";
+import {useNavigate} from "react-router-dom";
+import { useState } from "react";
