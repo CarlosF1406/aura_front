cf2f70f86bce0ad19dce19d62ec6ac8387537da5
Warloy <warloy989@gmail.com>
2024-10-10 18:35:54 -0400

    Minor fix to style in Exp section

--- src/components/LandingComponents/ExperienceSection.tsx
+++ src/components/LandingComponents/ExperienceSection.tsx
@@ -14,21 +14,21 @@ export function ExperienceSection() {
     >
       <div
         aria-label="Information"
-        className="flex flex-col columns-1 gap-4 justify-center items-center h-full xs:w-screen md:w-1/2"
+        className="flex flex-col columns-1 gap-4 justify-center items-center h-full xs:w-screen md:w-1/2 z-10"
       >
         <p className="w-2/3 text-left text-6xl md:text-8xl font-bold text-wrap">EN AURA PERSONAS Y TECNOLOGÍA</p>
         <p className="w-2/3 text-center text-3xl font-bold text-wrap">Nos unimos para hacer de el sonido una experiencia inolvidable</p>
       </div>
       <div
         aria-label="Picture"
-        className="justify-center items-center h-full hidden md:flex md:w-1/2 pointer-events-none"
+        className="justify-center items-center h-full hidden md:flex md:w-1/2 pointer-events-none z-0"
         style={{ maxWidth: "65%", transform: "scale(1.3)" }}
       >
         <Image
           alt={"assets experience"}
           src={assets_experience}
           style={{
-            maxHeight: `78vh`
+            maxHeight: `78vh`,
           }}
           className="pointer-events-none sm:hidden md:block"
         />
